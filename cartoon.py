d385c524ad0c7ac56a6c36f52007cf796f44d9fb
Denick Murray <denick@denick.org>
2017-12-23 15:54:29 +0100

    Add timeout and better img selection.

--- cartoon.py
+++ cartoon.py
@@ -6,10 +6,10 @@ from lxml import html
 COMIC_PAGE = 'http://www.dirkjan.nl'
 
 def fetch_comic():
-  page = requests.get(COMIC_PAGE).text
+  page = requests.get(COMIC_PAGE, timeout=5).text
   doc = html.fromstring(page)
   link = doc.cssselect("article")[0]
-  img = link.getchildren()[0]
+  img = link.cssselect('img')[0]
   return img.attrib['src']
 
 def main():
